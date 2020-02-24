f542b702420775cea7e802bb119c0429b9c21f09
棋策 <lf184858@antfin.com>
2020-02-24 10:57:03 +0800

    select_url sort by delay time

--- select_url.sh
+++ select_url.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/env sh
 
+logfile='ping.log'
+
+: > $logfile
+
 sed -E 's#https?:##;s#/##g' url | while read line
 do
-	ping -c 10 -n $line
-	echo
+	ping -c 10 -n $line >> $logfile
+	echo >> $logfile
 done
 
-
+awk '/statistics/{
+	domain[NR] = $2
+}
+/transmitted/{
+	loss_rate[NR-1] = $7
+}
+/round-trip/{
+	split($4, delays, "/")
+	avg[NR-2] = delays[2]
+}
+END {
+	for(key in domain) {
+		print key, domain[key], loss_rate[key], avg[key]
+	}
+}
+' $logfile | sort -k4 -n
